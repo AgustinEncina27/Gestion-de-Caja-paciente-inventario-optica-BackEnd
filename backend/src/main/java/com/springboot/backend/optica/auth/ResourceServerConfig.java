0fafa0566e37b0aa60960e413e7e60abe8a3e0db
Encina Agustin <agustin72encina@gmail.com>
2025-06-24 20:04:20 -0300

    cambios en el metodo de busqueda en crear movimiento 4

--- backend/src/main/java/com/springboot/backend/optica/auth/ResourceServerConfig.java
+++ backend/src/main/java/com/springboot/backend/optica/auth/ResourceServerConfig.java
@@ -31,8 +31,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config= new CorsConfiguration();
-		config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost"));
-		//config.setAllowedOrigins(Arrays.asList("https://opticafabricioaquino.com.ar","https://www.opticafabricioaquino.com.ar"));
+		//config.setAllowedOrigins(Arrays.asList("http://localhost:4200", "http://localhost"));
+		config.setAllowedOrigins(Arrays.asList("https://opticafabricioaquino.com.ar","https://www.opticafabricioaquino.com.ar"));
 		config.setAllowedMethods(Arrays.asList("GET","POST","PUT","DELETE","OPTIONS"));
 		config.setAllowCredentials(true);
 		config.setAllowedHeaders(Arrays.asList("Content-Type","Authorization"));
