121856bca545e3877c479479399b13eac56f6bca
Encina Agustin <agustin72encina@gmail.com>
2025-07-04 18:58:15 -0300

    cambios en en el reporte pdf

--- backend/src/main/java/com/springboot/backend/optica/service/IPdfService.java
+++ backend/src/main/java/com/springboot/backend/optica/service/IPdfService.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import com.springboot.backend.optica.modelo.Movimiento;
 
 public interface IPdfService {
-    byte[] generarReporteMovimiento(Movimiento movimiento) throws IOException;
     byte[] generarReporteMovimientoCliente(Movimiento movimiento) throws IOException;
     byte[] generarReporteMovimientoOptica(Movimiento movimiento) throws IOException;
 }
